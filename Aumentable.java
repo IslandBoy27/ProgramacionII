af09a4c3881ad2aa795e5647a3d7645f122169fa
IslandBoy27 <141108469+IslandBoy27@users.noreply.github.com>
2024-08-01 17:10:01 -0600

    Jaymar.
    Archivo de Aumentable

---
+++ Aumentable.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package ProgramacionII;
+
+public interface Aumentable {
+
+    void increaseIncome();
+
+    boolean validForIncrease();
+}
